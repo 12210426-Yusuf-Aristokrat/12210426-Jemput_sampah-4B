cb9a00bada8838fd9b43af99f67e099c45d9dbe8
RYAN <12210695@bsi.ac.id>
2023-06-26 01:12:20 +0700

    fitur lupa password

---
+++ lib/pages/lupa_password.dart
@@ -0,0 +1,61 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ForgotPasswordScreen extends StatefulWidget {
+  @override
+  _ForgotPasswordScreenState createState() => _ForgotPasswordScreenState();
+}
+
+class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+
+  void _resetPassword() async {
+    if (_formKey.currentState.validate()) {
+      try {
+        await FirebaseAuth.instance.sendPasswordResetEmail(
+          email: _emailController.text,
+        );
+        // Password reset email sent successfully
+        // Show a success message or navigate to a success screen
+      } catch (e) {
+        // Handle password reset errors
+        // Show an error message or provide feedback to the user
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lupa Password'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Tolong Masukkan Email';
+                  }
+                  // Add additional email validation if needed
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              RaisedButton(
+                child: Text('Atur Ulang Password'),
+                onPressed: _resetPassword,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

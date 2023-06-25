e4b5028c4975c8daeb8a74016b3a74189e79ff95
Yusuf Aristokrat <yusufaristokrat@gmail.com>
2023-06-26 00:57:39 +0700

    perubahan view

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:id_ac_bsi_4b_12210426_jemput_sampah/pages/contoh_provider.dart';
+import 'package:id_ac_bsi_4b_12210426_jemput_sampah/pages/home_view.dart';
 
 void main() {
   runApp(
      MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyApp(),
+      home: Navbar(),
     ),
   );
 }
